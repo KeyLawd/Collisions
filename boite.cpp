ef8478b8cf7db0b171edac003259974e66f03abe
KeyLawd <germain.vincen7@gmail.com>
2021-02-04 11:12:44 +0100

    Homogénéisation/fusion
    Fusion des fichiers temporaires dans boite . cpp et .h

--- boite.cpp
+++ boite.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-#include "main.h"
+
+const g=1;
+double distance_carre (x1,x2,y1,y2)
+{
+    return(pow(x2-x1,2)+pow(y2-y1,2));
+}
 using namespace std;
+int force ()
+{
+int somme =0;
+boite *suivant=boite_soeur;
+    while (boite_soeur!=NULL)
+    {
+          somme=somme+(suivant->masse*g)/distance_carre(xmass,ymass,suivant->xmass,suivant->ymass));
+          suivant=boite_soeur->boite_soeur;
+    }
+    return(somme);
+}
 
 void ajout_part(boite * B, particule * p)
 {
